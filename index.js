a4e3976097ef4eac8dce0314814926c52759f55a
Nikhil Singh <nikhils@vega6.com>
2024-09-03 11:19:55 +0530

    new port added

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
 });
